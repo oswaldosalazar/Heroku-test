18e99ea208753e09a5fe66f24e7be485d50175a4
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2016-08-25 14:36:39 -0600

    Master updaed again

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-      title: '"Working on new things"',});
+      title: 'Express again',});
 });
 
 module.exports = router;
